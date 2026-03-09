dde9e1481792b4792dea5815911977192fc2115b
Your Name <your-email@example.com>
2026-03-09 20:29:24 +0530

    fix inference model loading

--- inference/predict.py
+++ inference/predict.py
@@ -1,20 +1,14 @@
 import torch
-from huggingface_hub import hf_hub_download
 from inference.model import ChessPolicyNet
 
 
 class ChessInference:
-    def __init__(self, device=None):
+    def __init__(self, model_path: str, device=None):
         self.device = device or torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
 
         self.model = ChessPolicyNet().to(self.device)
-
-        model_path = hf_hub_download(
-            repo_id="VishnuRaju06/chess-bot-model",
-            filename="chess_model.pth"
-        )
 
         self.model.load_state_dict(
             torch.load(model_path, map_location=self.device)
